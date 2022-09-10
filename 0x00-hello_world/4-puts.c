603e9ffa55caf7f81c4622ba1ef0595e2cd832bd
Chioma Williams <chiomawilliams001@gmail.com>
2022-09-09 19:51:32 -0700

    Compiler

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - C program that prints
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: Always 0 (Successful)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
