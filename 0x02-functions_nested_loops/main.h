bd1e9120ac2ad759f796bd981a2e3f56689b69b4
Chioma Williams <chiomawilliams001@gmail.com>
2022-09-14 05:02:40 -0700

    Function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,33 +1,20 @@
-#ifndef HOLBERTON_H_INCLUDED
+#ifndef MAIN_H_CHY
+#define MAIN_H_CHY
 
-#define HOLBERTON_H_INCLUDED
+#include<stdio.h>
 
 Int _putchar(char);
-
 Void print_alphabet(void);
-
 Void print_alphabet_x10(void);
-
 Int _islower(int c);
-
 Int _isalpha(int c);
-
 Int print_sign(int n);
-
 Int _abs(int);
-
 Int print_last_digit(int);
-
 Void jack_bauer(void);
-
 Void times_table(void);
-
 Int add(int a, int b);
-
 Void print_to_98(int n);
-
 Void print_times_table(int n);
-
-Void num(int r, int n);
 
 #endif
