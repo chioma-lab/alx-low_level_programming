6ada2eda85f43404220255bc3434c1b67279cbfd
Chioma Williams <chiomawilliams001@gmail.com>
2022-09-16 19:42:00 -0700

    Debugging

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_numbers - prints numbers from 0 - 9 by using _putchar twice only
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
+}
