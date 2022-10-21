da0080f431ff3da0ba88114a0b1ec9e41e4fee1c
Chioma Williams <chiomawilliams001@gmail.com>
2022-10-21 10:55:39 -0700

    stringly

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -9,7 +12,9 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for ALX project
  */
+
 typedef struct list_s
 {
 	char *str;
@@ -22,6 +27,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void racefirst(void) __attribute__ ((constructor));
 
-#endif /* MAIN_H */
+#endif /* LISTS_H */
