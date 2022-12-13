a3c49b4eb73beaca00101918f4cbc08a6165fbf2
Chioma Williams <chiomawilliams001@gmail.com>
2022-12-13 11:40:24 -0800

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Chynet35/alx-low_level_programming/master/0x18-dynamic_libraries/preload.so
+wget -P /tmp/ https://github.com/chioma-lab/alx-low_level_programming/master/0x18-dynamic_libraries/preload.so
 export LD_PRELOAD=/tmp/preload.so
